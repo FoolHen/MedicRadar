3fe845ecd4489faae0b2fee85c93c55841279206
Raikem <fulgen_3_7ad@hotmail.com>
2017-09-07 21:52:43 +0200

    Moved logic to client. Added logic to show only players with defib

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -1,13 +1,15 @@
 class "MedicRadarClient"
 
 function MedicRadarClient:__init()
-  self.m_GetMedics = NetEvents:Subscribe('medicradar:nearbymedics', self, self.OnGetMedics)
-  self.m_ClearMR = NetEvents:Subscribe('medicradar:clearui', self, self.OnClearUI)
+  self.m_ClearUI = NetEvents:Subscribe('medicradar:clearui', self, self.OnClearUI)
   self.m_ShowUI = NetEvents:Subscribe('medicradar:showui', self, self.OnShowUI)
   self.m_OnLoadedEvent = Events:Subscribe('ExtensionLoaded', self, self.OnLoaded)
-  self.m_ClientUpdateInputEvent = Events:Subscribe('Client:UpdateInput', self, self.OnUpdateInput)
-
+  self.m_ClientFrameUpdateEvent = Events:Subscribe('Client:PostFrameUpdate', self, self.OnPostFrameUpdate)
   self.m_Medics = {}
+  self.m_DeathPos = nil
+  self.m_IsClientDead = false
+  self.m_DeadTimer = 0
+  self.test = 0
 end
 
 function MedicRadarClient:OnLoaded()
@@ -16,11 +18,7 @@ function MedicRadarClient:OnLoaded()
   WebUI:Hide()
 end
 
-function MedicRadarClient:OnGetMedics(p_Medics)
- -- gets name and distance of nerby medics and calculate
- -- display with webui
-
-  --print("Recived call from server nearbymedics")
+function MedicRadarClient:UpdateUI(p_Medics)
   WebUI:ExecuteJS('removeAll()')
 
   local s_Messages = split(p_Medics, "|")
@@ -33,14 +31,82 @@ function MedicRadarClient:OnGetMedics(p_Medics)
 end
   
 function MedicRadarClient:OnClearUI()
-  print("Recived call from server clearui")
+  print("Recived call from server: clearui")
+  self.m_IsClientDead = false
+  self.m_DeadTimer = 0
   WebUI:ExecuteJS('removeAll()')
   WebUI:Hide()
+
 end
 
-function MedicRadarClient:OnShowUI()
-  print("Recived call from server showui")
+function MedicRadarClient:OnShowUI(p_Position)
+  print("Recived call from server: showui")
+  self.m_DeathPos = p_Position
+  self.m_IsClientDead = true
+  print("got pos: " .. tostring(p_Position.x) .. " ".. tostring(p_Position.y) .. " ".. tostring(p_Position.z) .. " ")
+  
+end
+
+function MedicRadarClient:OnPostFrameUpdate(p_Delta)
+  if not self.m_IsClientDead then 
+    return
+  end
+
+  self.m_DeadTimer = self.m_DeadTimer + p_Delta
+
+  if self.m_DeadTimer < 1 then  
+    return
+  end
+
+  local s_LocalPlayer = PlayerManager:GetLocalPlayer()
+
+  if s_LocalPlayer == nil then
+    return
+  end
+  
+  local s_LocalSoldier = s_LocalPlayer.soldier
+
+  if s_LocalSoldier ~= nil then
+    self:OnClearUI()
+    return
+  end
+
   WebUI:Show()
+
+  local s_Players = PlayerManager:GetPlayers()
+  local s_MedicsWithDefib = ""
+
+  for s_Index, s_Player in pairs(s_Players) do 
+        
+    if s_LocalPlayer.name ~= s_Player.name and -----------------------------
+      s_LocalPlayer.teamID == s_Player.teamID then
+
+      --print("found player in same team")
+      local s_Soldier = s_Player.soldier
+      
+      if s_Soldier ~= nil then
+
+        if self:HasDefib(s_Soldier) then
+          local distance2 =  (self.m_DeathPos.x - s_Soldier.transform.trans.x) * (self.m_DeathPos.x - s_Soldier.transform.trans.x) + 
+            (self.m_DeathPos.y - s_Soldier.transform.trans.y) * (self.m_DeathPos.y - s_Soldier.transform.trans.y)
+          local distance = math.floor( math.sqrt(distance2) )
+
+          s_MedicsWithDefib = s_MedicsWithDefib .. s_Player.name .. ": " .. tostring(distance) .. "m.|"
+        end
+      end
+    end
+  end
+  self:UpdateUI(s_MedicsWithDefib)
+end
+
+function MedicRadarClient:HasDefib(p_Soldier)
+  local s_Gadget = tostring(s_Soldier:GetWeaponNameByIndex(5)):lower()
+
+  if s_Gadget == "defib" then
+    return true
+  end
+
+  return false
 end
 
 function split(pString, pPattern)
@@ -62,21 +128,5 @@ function split(pString, pPattern)
    return Table
 end
 
-function MedicRadarClient:OnUpdateInput(p_Delta)
-  if InputManager:WentKeyDown(InputDeviceKeys.IDK_F1) then
-    local s_Player = PlayerManager:GetLocalPlayer()
-    if s_Player == nil then
-      return
-    end
-    
-    local s_Soldier = s_Player.soldier
-       if s_Soldier == nil then
-      return
-    end
-    print(tostring(s_Soldier:GetWeaponNameByIndex(5)))
-
-  end
-
-end
   
 g_MedicRadarClient = MedicRadarClient()
--- ext/Server/__init__.lua
+++ ext/Server/__init__.lua
@@ -2,96 +2,37 @@ class "MedicRadarServer"
 
 function MedicRadarServer:__init()
 	self.m_PlayerKilledEvent = Events:Subscribe("Player:Killed", self, self.OnPlayerKilled)
-  self.m_UpdateEvent = Events:Subscribe("Engine:Update", self, self.OnUpdate)
-
-  self.m_DeadPlayers = {}
-  self.m_Timer = 0.0	
-  self.test = 0
+  self.m_PlayerDeletedEvent = Events:Subscribe("Player:Deleted", self, self.OnPlayerDeleted)
+  self.m_PlayerDestroyedEvent = Events:Subscribe("Player:Destroyed", self, self.OnPlayerDestroyed)
+  self.m_PlayerTeamChangeEvent = Events:Subscribe("Player:TeamChange", self, self.OnPlayerTeamChange)
 end
 
-function MedicRadarServer:OnUpdate(p_Delta, p_SimulationDelta) --shitty code but works
 
-  self.m_Timer = self.m_Timer + p_Delta
-
-  if self.m_Timer < 1 then --check every second
-    return
-  end
-
-  self.m_Timer = 0
-
-  local s_Players = PlayerManager:GetPlayers()
-
-  -- Select a dead player waiting for revive
-  for s_Index, s_Victim in pairs(self.m_DeadPlayers) do 
-    local s_MedicsWithDefib = ""
-
-    if s_Victim.player == nil or s_Victim.player.soldier ~= nil or
-        s_Victim.time <= 0 then
-      --Delete player from table and hide his UI
-      self.m_DeadPlayers[s_Index] = nil
-      NetEvents:SendTo('medicradar:clearui', s_Victim.player )
-      goto continue
-    end
-
-    s_Victim.time = s_Victim.time - 1
-    --print("time: " .. tostring(s_Victim.time))
-
-    -- Check all players that have defib and are on the same team as the victim
-    for j, s_Player in pairs(s_Players) do 
-      
-      if s_Victim.player.name ~= s_Player.name and -----------------------------
-        s_Victim.player.teamID == s_Player.teamID then
-
-        --print("found player in same team")
-        local s_Soldier = s_Player.soldier
-        
-        if s_Soldier ~= nil  then and tostring(s_Soldier:GetWeaponNameByIndex(5)) == Defib
-          --self:CheckIfMedic(s_Soldier) 
-          print("soldier alive with defib")
-
-          local distance2 =  (s_Victim.x - s_Soldier.transform.trans.x) * (s_Victim.x - s_Soldier.transform.trans.x) + 
-            (s_Victim.z - s_Soldier.transform.trans.z) * (s_Victim.z - s_Soldier.transform.trans.z)
-          local distance = math.floor( math.sqrt(distance2) )
-
-          s_MedicsWithDefib = s_MedicsWithDefib .. s_Player.name .. ": " .. tostring(distance) .. "m.|"
-        -- else --just for testing --------------------
-        --   self.test = self.test + 1
-        --   local distance2 = (s_Victim.x - 15 + self.test) * (s_Victim.x - 15+ self.test) +  (s_Victim.z - 15+ self.test)*(s_Victim.z - 15+ self.test)
-        --   local distance = math.floor( math.sqrt(distance2) )
-
-        --   s_MedicsWithDefib = s_MedicsWithDefib .. s_Player.name .. ": " .. tostring(distance) .. "m.|"
-        --   s_MedicsWithDefib = s_MedicsWithDefib .. "topotamadre" .. ": " .. tostring(self.test) .. "m.|"
-        end
-      end
-    end
-    
-    NetEvents:SendTo('medicradar:nearbymedics', s_Victim.player, s_MedicsWithDefib)
-    s_MedicsWithDefib = nil
-
-    ::continue::
-  end
-end
-
---check if player is medic and has a defib
-function MedicRadarServer:CheckIfMedic(p_Soldier)
-  
-  --TODO
-  
-end
 
 function MedicRadarServer:OnPlayerKilled(p_Victim, p_Inflictor, p_Position, p_Weapon, p_RoadKill, p_HeadShot, p_VictimInReviveState)  
   if p_Victim == nil then
 		return
 	end
-  print(p_VictimInReviveState)
-  -- if p_VictimInReviveState == false then --------------
-  --   return
-  -- end
-  
-  print("Victim: " .. tostring(p_Victim.name))
-  table.insert(self.m_DeadPlayers,{player = p_Victim, time = 10, x = p_Position.x, z = p_Position.z})
-  --self.m_DeadPlayers[p_Victim] = {time = 5}
-  NetEvents:SendTo('medicradar:showui', p_Victim)
+
+  --print("Victim: " .. tostring(p_Victim.name))
+  NetEvents:SendTo('medicradar:showui', p_Victim, p_Position)
+
+end
+
+function MedicRadarServer:OnPlayerDeleted(p_Player)
+  --print(p_Player.name .. " OnPlayerDeleted")
+  NetEvents:SendTo('medicradar:clearui', p_Victim)
+end
+
+function MedicRadarServer:OnPlayerDestroyed(p_Player)
+  --print(p_Player.name .. " OnPlayerDestroyed")
+  NetEvents:SendTo('medicradar:clearui', p_Victim)
+
+end
+
+function MedicRadarServer:OnPlayerTeamChange(p_Player)
+  --print(p_Player.name .. " OnPlayerTeamChange")
+  NetEvents:SendTo('medicradar:clearui', p_Victim)
 
 end
 
