51b54a4bc70432a2004421f017c1df5e93ca467b
Raikem <fulgen_3_7ad@hotmail.com>
2017-09-12 20:24:03 +0200

    Fixed resizing of text completely

--- www/script/script.js
+++ www/script/script.js
@@ -2,15 +2,16 @@ var num=0;
 
 function addText(text) {
     var res = text.split("|");
-    var limit = Math.min(res.length, 9);
+    var limit = Math.min(res.length-1, 9);
 
     removeAll();
     
     for (var j = 0;j < limit; j++) {
         var listed = '<div id="item' + (j+1) + '"><label>' + res[j] + '</label></div>';
-        document.getElementById("list").innerHTML += listed;
+        document.getElementById("page").innerHTML += listed;
     }
-    num = res.length;
+    num = limit;
+    fontsize();
 }
 function removeAll(){
     for (var i = num; i > 0; i--) {
@@ -18,3 +19,10 @@ function removeAll(){
     }
     num = 0;
 }
+fontsize = function () {
+    var size = $("#orangeRect").width() * 0.06;
+    $('label').css('font-size', size);
+};
+
+$(window).resize(fontsize);
+$(document).ready(fontsize);
